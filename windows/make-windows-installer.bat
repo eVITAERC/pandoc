e0e52bf01842dfa4d60a80de69e3ab7e8f0e3bfd
Tim Lin <timtylin@gmail.com>
2014-11-04 00:14:25 -0800

    Update Windows build scripts for Scholdoc
    In addition, added compiled COPYING to the windows distro directory
    since Scholdoc can't output to RTF
    
    [skip ci]

--- windows/make-windows-installer.bat
+++ windows/make-windows-installer.bat
@@ -5,15 +5,16 @@ cabal sandbox init
 cabal clean
 cabal install hsb2hs
 if %errorlevel% neq 0 exit /b %errorlevel%
-cabal install -v1 --force --reinstall --flags="embed_data_files" . pandoc-citeproc
+cabal install -v1 --force --reinstall --flags="embed_data_files" . scholdoc-citeproc
 if %errorlevel% neq 0 exit /b %errorlevel%
-strip .\.cabal-sandbox\bin\pandoc.exe
-strip .\.cabal-sandbox\bin\pandoc-citeproc.exe
+strip .\.cabal-sandbox\bin\scholdoc.exe
+strip .\.cabal-sandbox\bin\scholdoc-citeproc.exe
 copy COPYRIGHT COPYRIGHT.txt
-for /f "tokens=1-2 delims= " %%a in ('.\.cabal-sandbox\bin\pandoc --version') do (
-  @set VERSION=%%b
-  goto :next
-  )
+:: for /f "tokens=1-2 delims= " %%a in ('.\.cabal-sandbox\bin\scholdoc --version') do (
+::   @set VERSION=%%b
+::   goto :next
+::   )
+@set VERSION=0.1.3
 :next
 if "%VERSION%" == "" (
   echo Error: could not determine version number.
@@ -22,9 +23,9 @@ if "%VERSION%" == "" (
 echo Detected version %VERSION%
 cd windows
 echo Creating msi...
-candle -dVERSION=%VERSION% pandoc.wxs
+candle -dVERSION=%VERSION% scholdoc.wxs
 if %errorlevel% neq 0 exit /b %errorlevel%
-light  -sw1076 -ext WixUIExtension -out pandoc-%VERSION%-windows.msi pandoc.wixobj
+light  -sw1076 -ext WixUIExtension -out scholdoc-%VERSION%-alpha-windows.msi scholdoc.wixobj
 if %errorlevel% neq 0 exit /b %errorlevel%
-echo Starting kSign: sign, then quit kSign to complete the build...
-kSign
+:: echo Starting kSign: sign, then quit kSign to complete the build...
+:: kSign
